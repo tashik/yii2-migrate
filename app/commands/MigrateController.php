148b673592e79b4071cb09d420a687ff9fdaf16c
tashik <patrinat@gmail.com>
2016-11-01 12:39:35 +0300

    все параметры передаем в шаблон

--- app/commands/MigrateController.php
+++ app/commands/MigrateController.php
@@ -62,8 +62,7 @@ class MigrateController extends \yii\console\controllers\MigrateController
     $file = $this->getMigrationFiles()[$class];
     require_once($file);
     $params = Yii::$app->params;
-    $dbUsers = (isset($params['dbUsers'])) ? $params['dbUsers'] : array();
-    return new $class(['db' => $this->db], $dbUsers );
+    return new $class(['db' => $this->db], $params );
   }
 
   protected function getNewMigrations()
